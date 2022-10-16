4dd23495b218e3abc5d3ab436ab27f1fb496f4ee
Oporu <a0908017366@gmail.com>
2022-10-16 21:36:40 +0800

    modified:   homework4/index.js

--- homework4/index.js
+++ homework4/index.js
@@ -1,6 +1,6 @@
 var rotate = 0;
 div = document.getElementById("box");
 document.onclick = function(){
-	rotate = rotate+1%360;
+	rotate = (rotate+1)%360;
 	div.style.transform = "rotate("+rotate.toString()+"deg)";
 }
