22217af58369aa77c723c2fedcb588bf1a378612
Oporu <a0908017366@gmail.com>
2022-10-18 22:56:56 +0800

    modified:   homework2/index.js 	modified:   homework4/style.css 	modified:   homework5/index.html 	modified:   index.html

--- homework2/index.js
+++ homework2/index.js
@@ -1,29 +1,22 @@
-html = document.documentElement;
 img = document.getElementById("img");
-
-html.onclick = function(){
+document.documentElement.onclick = function(){
 	window.open("../","_self");
 }
 var i = 0;
-var s = 0;
-var r1 = 0;
-var g1 = 0;
+var a1 = 0;
 var b1 = 0;
-var r2 = 130;
-var g2 = 130;
+var a2 = 130;
 var b2 = 130;
-looping = function(){
+function looping(){
 	i=(i+0.01)%360;
 	sin = Math.sin(i);
 	cos = Math.cos(i);
-	r1 = 150*sin + 50;
-	g1 = 150*sin + 150;
+	a1 = 150*sin + 50;
 	b1 = 150*sin + 150;
-	r2 = 150*cos + 50;
-	g2 = 150*cos + 150;
+	a2 = 150*cos + 50;
 	b2 = 150*cos + 150;
-	html.style.backgroundImage = "linear-gradient(rgba("+r1.toString()+", "+g1.toString()+", "+b1.toString()+", 0.5), rgba("+r2.toString()+", "+g2.toString()+", "+b2.toString()+", 0.5))";
-	img.style.transform = "rotate("+(Math.sin(i)*180+180).toString()+"deg)";
+	document.documentElement.style.backgroundImage = "linear-gradient(rgba("+a1.toString()+", "+b1.toString()+", "+b1.toString()+", 0.5), rgba("+a2.toString()+", "+b2.toString()+", "+b2.toString()+", 0.5))";
+	img.style.transform = "rotate("+((Math.sin(i) + 1) * 180).toString()+"deg)";
 	setTimeout(looping, 10);
 }
 looping();
