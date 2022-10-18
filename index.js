fc6369f205b49c68d5ce0c9d527da9f5410528a5
Oporu <a0908017366@gmail.com>
2022-10-18 17:17:55 +0800

    modified:   index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 function rickroll(){
 	window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ","_blank");
 }
+var i = 0;
+var i2 = 0;
 function looping(){
-	var i = 0;
-	var i2 = 0;
-	
 	i = (i+0.0125)%360;
 	i2 = (i2+0.5)%360;
 	r1 = 127*(1+Math.sin(i));
