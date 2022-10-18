b290f03b6f631491b4c7d9aa71e2370fb555f294
Oporu <a0908017366@gmail.com>
2022-10-18 17:05:41 +0800

    modified:   index.js

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ document.onkeydown = rickroll;
 function looping(){
 	i = (i+0.0125)%360;
 	i2 = (i2+0.5)%360;
-	r1 = 127*Math.sin(i) + 127;
-	r2 = 127*Math.cos(i) + 127;
+	r1 = 127*(1+Math.sin(i));
+	r2 = 127*(1+Math.cos(i));
 	html.style.backgroundImage = "linear-gradient("+(i2).toString()+"deg,rgba("+r1.toString()+", "+r1.toString()+", "+(100).toString()+", 0.5), rgba("+r2.toString()+", "+r2.toString()+", "+(100).toString()+", 0.5)), url(\"./background.png\")";
 	setTimeout(looping, 10);
 }
