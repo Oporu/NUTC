8f51bae616e1a116b89f3abe4c326bf0ae39560e
Oporu <a0908017366@gmail.com>
2022-10-20 20:07:55 +0800

    modified:   script.js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-void function rickroll(){
+function rickroll(){
 	window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ","_blank");
 }
-var i = 0;
-var i2 = 0;
-void function looping(){
-	i = (i+0.0125)%360;
-	i2 = (i2+0.5)%360;
-	r1 = 127*(1+Math.sin(i));
-	r2 = 127*(1+Math.cos(i));
-	document.documentElement.style.backgroundImage = "linear-gradient("+(i2).toString()+"deg,rgba("+r1.toString()+", "+r1.toString()+", "+(100).toString()+", 0.5), rgba("+r2.toString()+", "+r2.toString()+", "+(100).toString()+", 0.5)), url(\"./background.png\")";
+{looping.i = 0; looping.i2 = 0} function looping(){
+	looping.i = (looping.i+0.0125)%360;
+	looping.i2 = (looping.i2+0.5)%360;
+	r1 = 127*(1+Math.sin(looping.i));
+	r2 = 127*(1+Math.cos(looping.i));
+	document.documentElement.style.backgroundImage = "linear-gradient("+(looping.i2).toString()+"deg,rgba("+r1.toString()+", "+r1.toString()+", "+(100).toString()+", 0.5), rgba("+r2.toString()+", "+r2.toString()+", "+(100).toString()+", 0.5)), url(\"./background.png\")";
 	setTimeout(looping, 10);
 }
 document.onkeydown = rickroll;
