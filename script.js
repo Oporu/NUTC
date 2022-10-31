3f05b6181f1d45859ead65ddfd69536a5c121dd9
Oporu <a0908017366@gmail.com>
2022-10-31 12:45:30 +0800

    modified:   script.js

--- script.js
+++ script.js
@@ -18,7 +18,6 @@ fetch("https://api.countapi.xyz/hit/oporu.github.io/NUTC/")
 		document.getElementById("visits").innerText = data.value + " visits";
 });
 Promise.all(Array.from(document.images).filter(img => !img.complete).map(img => new Promise(resolve => { img.onload = img.onerror = resolve; }))).then(() => {
-    console.log('images finished loading');
 	document.getElementById("loading").style.display = "none";
 	document.getElementById("content").style.display = "block";
 });
