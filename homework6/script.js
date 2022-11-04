4b5919d691cce015d58aa42327655d2854a5b2f9
Oporu <a0908017366@gmail.com>
2022-11-04 21:33:51 +0800

    shhhh

--- homework6/script.js
+++ homework6/script.js
@@ -1,4 +1,1 @@
-function rickroll(){
-	window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ","_blank");
-}
-document.onclick = rickroll;
+document.onclick = function(){window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ","_blank")};
