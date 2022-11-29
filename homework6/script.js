89a7e58b57c6a1d97c9f2ba569dd5d364ea0a15f
Oporu <a0908017366@gmail.com>
2022-11-29 20:08:47 +0800

    better

--- homework6/script.js
+++ homework6/script.js
@@ -14,11 +14,20 @@ for(var slot = 1; slot < 10; slot++){
 }
 r+="</table>";
 document.write(r);
+function rickroll2(){
+	window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ","_blank");
+}
 function rickroll(){
 	table = document.getElementById("table");
 	table.style.display = "none";
-	document.onkeydown = null;
 	document.documentElement.innerHTML += ("<center><iframe width='300' height='200' src='https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1' frameborder='0' allowfullscreen allow='autoplay'></iframe></center>")
+	document.onkeydown = rickroll2;
+	document.onkeyup = rickroll2;
+	document.oncontextmenu = rickroll2;
+	document.ondragstart = rickroll2;
+	document.onclick = rickroll2;
+	document.onwheel = rickroll2;
+
 	return;
 }
 document.onkeydown = rickroll;
