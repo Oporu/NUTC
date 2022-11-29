bba628225816b3a2ee403b98ba10eae2fb27a29c
Oporu <a0908017366@gmail.com>
2022-11-30 00:03:54 +0800

    tooltip

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-function rickroll(){window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ","_blank");}{looping.i = 0; looping.i2 = 0} function looping(){looping.i = (looping.i+0.0125)%360;looping.i2 = (looping.i2+0.5)%360;r1 = 127*(1+Math.sin(looping.i));r2 = 127*(1+Math.cos(looping.i));/*document.documentElement.style.backgroundImage = "linear-gradient("+(looping.i2).toString()+"deg,rgba("+r1.toString()+", "+r1.toString()+", "+(100).toString()+", 0.5), rgba("+r2.toString()+", "+r2.toString()+", "+(100).toString()+", 0.5)), url(\"./background.png\")";document.documentElement.style.backgroundImage = "conic-gradient(rgba("+r1.toString()+", "+r1.toString()+", "+(100).toString()+", 0.5), rgba("+r2.toString()+", "+r2.toString()+", "+(100).toString()+", 0.5)), url(\"./background.png\")";*/document.documentElement.style.backgroundImage = "radial-gradient(rgba("+r1.toString()+", "+r1.toString()+", "+(100).toString()+", 0.5), rgba("+r2.toString()+", "+r2.toString()+", "+(100).toString()+", 0.5)), url(\"./background.png\")";setTimeout(looping, 10);}document.onkeydown = rickroll;document.oncontextmenu = rickroll;document.ondragstart = rickroll;looping();fetch("https://api.countapi.xyz/hit/oporu.github.io/NUTC/").then(function(response) {return response.json();}).then(function(data) {document.getElementById("visits").innerText = data.value + " visits";});
+function rickroll(){
+	window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ","_blank");
+}
+{looping.i = 0; looping.i2 = 0} function looping(){
+	looping.i = (looping.i+0.0125)%360;
+	looping.i2 = (looping.i2+0.5)%360;
+	r1 = 127*(1+Math.sin(looping.i));
+	r2 = 127*(1+Math.cos(looping.i));
+	/*
+	document.documentElement.style.backgroundImage = "linear-gradient("+(looping.i2).toString()+"deg,rgba("+r1.toString()+", "+r1.toString()+", "+(100).toString()+", 0.5), rgba("+r2.toString()+", "+r2.toString()+", "+(100).toString()+", 0.5)), url(\"./background.png\")";
+	document.documentElement.style.backgroundImage = "conic-gradient(rgba("+r1.toString()+", "+r1.toString()+", "+(100).toString()+", 0.5), rgba("+r2.toString()+", "+r2.toString()+", "+(100).toString()+", 0.5)), url(\"./background.png\")";
+	*/
+	document.documentElement.style.backgroundImage = "radial-gradient(rgba("+r1.toString()+", "+r1.toString()+", "+(100).toString()+", 0.5), rgba("+r2.toString()+", "+r2.toString()+", "+(100).toString()+", 0.5)), url(\"./background.png\")";
+	
+	setTimeout(looping, 10);
+}
+document.onkeydown = rickroll;
+document.oncontextmenu = rickroll;
+document.ondragstart = rickroll;
+looping();
+fetch("https://api.countapi.xyz/hit/oporu.github.io/NUTC/")
+	.then(function(response) {
+		return response.json();
+	}).then(function(data) {
+		document.getElementById("visits").innerText = data.value + " visits";
+});
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
