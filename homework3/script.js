d0a02d157287a9e624850f45d719a477f9ffd2cb
Oporu <a0908017366@gmail.com>
2022-12-07 14:42:42 +0800

    better

--- homework3/script.js
+++ homework3/script.js
@@ -3,7 +3,7 @@ var theTableThing = 0;
 function theTableUpdate(){
 	theTableThing = ((theTableThing + 0.01) % 360);
 	theTable.width = (500 + (Math.sin(theTableThing) * 200)).toString() + "px";
-	(Array.prototype.slice.call(document.getElementsByTagName("td"))).forEach((element,index)=>{
+	[...document.getElementsByTagName("td")].forEach((element,index)=>{
 		if (index%2==0)
 			element.style.transform = "rotate("+(Math.cos(theTableThing)*180+180).toString()+"deg)";
 		else
