05333bdbf92392f79d188bf844078cd2d33404e5
Oporu <a0908017366@gmail.com>
2022-12-07 14:39:05 +0800

    new way

--- homework3/script.js
+++ homework3/script.js
@@ -1,14 +1,12 @@
 theTable = document.getElementById("theTable");
 var theTableThing = 0;
-var theTableThing2 = 0;
-theTableUpdate = function(){
+function theTableUpdate(){
 	theTableThing = ((theTableThing + 0.01) % 360);
 	theTable.width = (500 + (Math.sin(theTableThing) * 200)).toString() + "px";
-	for(i = 0; i < document.getElementsByTagName("td").length; i++){
-		if (i%2==0)
-			document.getElementsByTagName("td")[i].style.transform = "rotate("+(Math.cos(theTableThing)*180+180).toString()+"deg)";
+	(Array.prototype.slice.call(document.getElementsByTagName("td"))).forEach((element,index)=>{
+		if (index%2==0)
+			element.style.transform = "rotate("+(Math.cos(theTableThing)*180+180).toString()+"deg)";
 		else
-			document.getElementsByTagName("td")[i].style.transform = "rotate("+(Math.sin(theTableThing)*180+180).toString()+"deg)";
-	}
-	setTimeout(theTableUpdate,10);
+			element.style.transform = "rotate("+(Math.sin(theTableThing)*180+180).toString()+"deg)";
+	})
 }
