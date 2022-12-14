0d11df01bf238c1a41ca025bf89b58c6ede0c321
Oporu <a0908017366@gmail.com>
2022-12-14 13:42:20 +0800

    prepare for homework7

--- homework7/script.js
+++
@@ -1,1 +0,0 @@
-function rickroll(){window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ","_blank")}document.onclick=rickroll;document.onkeydown=rickroll;document.oncontextmenu=rickroll;document.ondragstart=rickroll;
